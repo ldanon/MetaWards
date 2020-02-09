ddd9bd6de670de022b47f6f6dfab5331bd6d0a9a
Leon <kernelblaha@gmail.com>
2020-02-09 09:49:34 +0000

    to be pulled and run fast on big laptop

--- Testing/run_repeats.sh
+++ Testing/run_repeats.sh
@@ -4,16 +4,19 @@
 # $2 is the parameters file name
 # $3 is the starting lines in the parameters file name
 # $COUNT is the line to be read from parameters file 
-
+ZERO=0
 COUNT=$3
 # echo $(($3+3))
 # bash until loop
-until [ $COUNT -gt $(($3+2)) ]; do
+until [ $COUNT -gt $(($3+20)) ]; do
     echo $COUNT
     mkdir $COUNT
     cd $COUNT
-    nohup ../$1 $RANDOM ../$2 $COUNT> /dev/null 
-    sleep 2
+#    nohup ../$1 $RANDOM ../$2 $COUNT> /dev/null 
+    echo $(pwd)
+    echo nohup ../$1 $RANDOM ../$2 $ZERO> /dev/null 
+    nohup ../$1 $RANDOM ../$2 $ZERO> /dev/null 
+#    sleep 2
     cd ..
     let COUNT=COUNT+1
 done
